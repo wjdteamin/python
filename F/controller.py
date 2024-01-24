2c55635d050530cce8f2e201d66005936e62fc2d
aaa <aaa@aa.com>
2024-01-24 13:19:56 +0900

    1월 24일(게시판)

--- F/controller.py
+++ F/controller.py
@@ -7,19 +7,19 @@ app = Flask(__name__)
 @app.route("/", methods=['get'])
 def list():
     value = d.findall()
-    return render_template("list.html", list=value)
+    return render_template("/list.html", list=value)
 
 
 @app.route("/read", methods=['get'])
 def read():
     bno = request.args.get("bno", type=int)
     value = d.findone(bno)
-    return render_template("read.html", read=value)
+    return render_template("/read.html", read=value)
 
 
 @app.route("/write", methods=['get'])
 def write():
-    return render_template("write.html")
+    return render_template("/write.html")
 
 
 @app.route("/write", methods=['post'])
@@ -38,4 +38,40 @@ def delete():
     return redirect("/")
 
 
+@app.route("/update", methods=['get'])
+def correction():
+    bno = request.args.get("bno", type=int)
+    return render_template("/update.html", bno=bno)
+
+
+@app.route("/update", methods=['post'])
+def update():
+    bno = request.form.get("bno", type=int)
+    title = request.form.get("title", type=str)
+    content = request.form.get("content", type=str)
+    d.update(bno=bno, title=title, content=content)
+    return redirect("/")
+
+
+# erd 설계 -> ???
+
+# 데이터 : model
+# 모델을 다루는 클래스 : DAO -> Data Access Object라고 한다. (DAO, 다오)
+
+# 상품 dao
+# 검색 : 전체출력, 상품하나의 상세 정보, 기능 별 검색, 인기 순 검색(top 검색), 가격 높은 순, 낮은 순 검색, 구매 많은 순, 적은 순
+# 분야 별 검색, 세일에 관한 검색,  내가 자주 구매했던 상품, 구매할만한 상품 -> 전부 다 read에 관한 것이다 .
+
+# 코딩을 하는 사람보다는 기획하는 사람이 더욱 더 중요하다.
+
+# 추가(save), 전체출력(findall), 하나출력(findone), 변경(update), 삭제(delete)
+
+# 글 : 글번호, 제목, 내용, 닉네임, 조회수
+
 app.run(debug=True)
+
+# 컨트롤러 구성
+# 1. 주소 지정(필수)
+# 2. request에서 값을 가져온다.
+# 3. dao를 부른다.
+# 4. html 또는 redirect를 사용한다.
--- F/dao.py
+++ F/dao.py
@@ -1,35 +1,91 @@
-board_list = []
-board_list.append(dict(bno=1, title="1번", nickname="하우스", views=0, content="집에 가고싶어요"))
-board_list.append(dict(bno=2, title="2번", nickname="이우스", views=0, content="집에 가고파"))
-board_list.append(dict(bno=3, title="3번", nickname="리우스", views=0, content="집에 그냥"))
-board_list.append(dict(bno=4, title="4번", nickname="한우스", views=0, content="집에 원해?"))
+# board_list = []
+# board_list.append(dict(bno=1, title="1번", nickname="하우스", views=0, content="집에 가고싶어요"))
+# board_list.append(dict(bno=2, title="2번", nickname="이우스", views=0, content="집에 가고파"))
+# board_list.append(dict(bno=3, title="3번", nickname="리우스", views=0, content="집에 그냥"))
+# board_list.append(dict(bno=4, title="4번", nickname="한우스", views=0, content="집에 원해?"))
 
-bno = 5
+# bno = 5
+
+
+# def findall():
+#     return board_list
+
+# # 작업을 2개 - 조회수 증가
+
+
+# def findone(bno):
+#     for b in board_list:
+#         if b['bno'] == bno:
+#             # 증가시키는 것이다.
+#             b['views'] = b['views']+1
+#             return b
+#     return False
+
+
+# def save(title: str, nickname: str, content: str) -> bool:
+#     global bno
+#     bol = dict(bno=bno, title=title, nickname=nickname, views=0, content=content)
+#     board_list.append(bol)
+#     bno += bno
+
+
+# def delete(bno: int) -> bno:
+#     for b in board_list:
+#         if b['bno'] == bno:
+#             board_list.remove(b)
+
+
+# erd 설계 -> ???
+
+# 데이터 : model
+# 모델을 다루는 클래스 : DAO -> Data Access Object라고 한다. (DAO, 다오)
+
+# 상품 dao
+# 검색 : 전체출력, 상품하나의 상세 정보, 기능 별 검색, 인기 순 검색(top 검색), 가격 높은 순, 낮은 순 검색, 구매 많은 순, 적은 순
+# 분야 별 검색, 세일에 관한 검색,  내가 자주 구매했던 상품, 구매할만한 상품 -> 전부 다 read에 관한 것이다 .
+
+# 코딩을 하는 사람보다는 기획하는 사람이 더욱 더 중요하다.
+
+# 추가(save), 전체출력(findall), 하나출력(findone), 변경(update), 삭제(delete)
+
+# 글 : 글번호, 제목, 내용, 닉네임, 조회수
+
+
+board_list = []
+bno = 1
 
 
 def findall():
     return board_list
 
-# 작업을 2개 - 조회수 증가
-
 
 def findone(bno):
-    for b in board_list:
-        if b['bno'] == bno:
-            # 증가시키는 것이다.
-            b['views'] = b['views']+1
-            return b
+    for board in board_list:
+        if board['bno'] == bno:
+            board['views'] += 1
+            return board
     return False
 
 
-def save(title: str, nickname: str, content: str) -> bool:
+def save(title, content, nickname):
     global bno
-    bol = dict(bno=bno, title=title, nickname=nickname, views=0, content=content)
+    bol = dict(bno=bno, title=title, content=content, nickname=nickname, views=0)
     board_list.append(bol)
-    bno += bno
+    bno += 1
+    return True
 
 
-def delete(bno: int) -> bno:
-    for b in board_list:
-        if b['bno'] == bno:
-            board_list.remove(b)
+def update(bno, title, content):
+     for board in board_list:
+         if board['bno'] == bno:
+             board['title'] = title
+             board['content'] = content
+             return True
+
+
+def delete(bno):
+    for board in board_list:
+        if board['bno'] == bno:
+            board_list.remove(board)
+            return True
+    return False
